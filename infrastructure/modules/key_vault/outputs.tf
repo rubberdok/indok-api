ff42facaa9fb3e49d63c9e98f4f7f4ed9df0bc61
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-03 00:56:20 +0200

    chore: re-add data

--- infrastructure/modules/key_vault/outputs.tf
+++ infrastructure/modules/key_vault/outputs.tf
@@ -1,7 +1,12 @@
 
+data "azurerm_key_vault_secret" "postgres_password" {
+  name         = "postgres_password"
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
 output "secrets" {
   value = {
-    "postgres_password" = azurerm_key_vault_secret.postgres_password.value
+    "postgres_password" = data.azurerm_key_vault_secret.postgres_password.value
   }
   sensitive = true
 }
