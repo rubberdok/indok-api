6754257d7ae642cb968dd44395aace3a53f7ee26
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-02 23:59:04 +0200

    Revert "chore: resource instead of data"
    This reverts commit 75c99150057c792aa3253076e6c60d932183e8f6.

--- infrastructure/modules/application/main.tf
+++ infrastructure/modules/application/main.tf
@@ -3,12 +3,12 @@ resource "azuread_application" "application" {
 }
 
 
-resource "azuread_service_principal" "service_principal" {
-  application_id = azuread_application.application.application_id
+data "azuread_service_principal" "service_principal" {
+  application_id = data.azuread_application.application.application_id
 }
 
 
 resource "azuread_application_password" "application_password" {
   display_name          = var.secret_name
-  application_object_id = azuread_application.application.object_id
+  application_object_id = data.azuread_application.application.object_id
 }
