13e77937bb91343d3beb24811c4449624a1ded68
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-03 00:52:24 +0200

    chore: more complexity in password

--- infrastructure/modules/key_vault/main.tf
+++ infrastructure/modules/key_vault/main.tf
@@ -12,8 +12,12 @@ resource "azurerm_key_vault" "key_vault" {
 }
 
 resource "random_password" "password" {
-  length  = 128
-  special = true
+  length      = 128
+  special     = true
+  min_lower   = 1
+  min_numeric = 1
+  min_upper   = 1
+  min_special = 1
 }
 
 resource "azurerm_key_vault_secret" "postgres_password" {
