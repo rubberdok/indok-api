3679c622d3028d0641826ae0dd335b3a02cac2b3
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-03 01:07:21 +0200

    chore: traditional access policy

--- infrastructure/modules/key_vault/main.tf
+++ infrastructure/modules/key_vault/main.tf
@@ -9,6 +9,29 @@ resource "azurerm_key_vault" "key_vault" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
   tenant_id           = var.tenant_id
+
+  access_policy = [
+    {
+      tenant_id = var.tenant_id
+      object_id = var.principal_id
+
+      key_permissions = [
+        "Get", "List",
+      ]
+      secret_permissions = ["Get", "List", ]
+    },
+    {
+      tenant_id = var.current_service_principal.tenant_id
+      object_id = var.current_service_principal.object_id
+
+      key_permissions = [
+        "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
+      ]
+      secret_permissions = [
+        "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set", "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set",
+      ]
+    },
+  ]
 }
 
 resource "random_password" "password" {
@@ -24,31 +47,4 @@ resource "azurerm_key_vault_secret" "postgres_password" {
   name         = "postgres-password"
   value        = random_password.password.result
   key_vault_id = azurerm_key_vault.key_vault.id
-
-  depends_on = [azurerm_key_vault_access_policy.current_principal, azurerm_key_vault_access_policy.principal]
-}
-
-resource "azurerm_key_vault_access_policy" "principal" {
-  key_vault_id   = azurerm_key_vault.key_vault.id
-  tenant_id      = var.tenant_id
-  object_id      = var.principal_id
-  application_id = var.application_id
-
-  key_permissions = [
-    "Get", "List",
-  ]
-  secret_permissions = ["Get", "List", ]
-}
-
-resource "azurerm_key_vault_access_policy" "current_principal" {
-  key_vault_id   = azurerm_key_vault.key_vault.id
-  tenant_id      = var.current_service_principal.tenant_id
-  object_id      = var.current_service_principal.object_id
-  application_id = var.application_id
-
-  key_permissions = [
-    "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
-  ]
-  secret_permissions  = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set", ]
-  storage_permissions = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update", ]
 }
