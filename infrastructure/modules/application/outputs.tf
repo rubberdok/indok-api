75c99150057c792aa3253076e6c60d932183e8f6
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-02 23:47:29 +0200

    chore: resource instead of data

--- infrastructure/modules/application/outputs.tf
+++ infrastructure/modules/application/outputs.tf
@@ -1,13 +1,13 @@
 output "principal_id" {
-  value = data.azuread_service_principal.service_principal.id
+  value = azuread_service_principal.service_principal.id
 }
 
 output "tenant_id" {
-  value = data.azuread_service_principal.service_principal.application_tenant_id
+  value = azuread_service_principal.service_principal.application_tenant_id
 }
 
 output "application_id" {
-  value = data.azuread_application.application.application_id
+  value = azuread_application.application.application_id
 }
 
 output "password" {
