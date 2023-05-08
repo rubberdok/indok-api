14683bb522ca3705b8971fb71c74787149911cb8
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-08 23:20:19 +0200

    chore: env variable instead of secret, add GH token

--- infrastructure/modules/github/main.tf
+++ infrastructure/modules/github/main.tf
@@ -12,23 +12,23 @@ resource "github_repository_environment" "environment" {
   repository  = var.repository_name
 }
 
-resource "github_actions_environment_secret" "azure_client_id" {
-  repository      = var.repository_name
-  secret_name     = "AZURE_CLIENT_ID"
-  environment     = github_repository_environment.environment.environment
-  plaintext_value = var.application_id
+resource "github_actions_environment_variable" "azure_client_id" {
+  repository    = var.repository_name
+  variable_name = "AZURE_CLIENT_ID"
+  environment   = github_repository_environment.environment.environment
+  value         = var.application_id
 }
 
-resource "github_actions_environment_secret" "azure_subscription_id" {
-  repository      = var.repository_name
-  secret_name     = "AZURE_SUBSCRIPTION_ID"
-  environment     = github_repository_environment.environment.environment
-  plaintext_value = var.subscription_id
+resource "github_actions_environment_variable" "azure_subscription_id" {
+  repository    = var.repository_name
+  variable_name = "AZURE_SUBSCRIPTION_ID"
+  environment   = github_repository_environment.environment.environment
+  value         = var.subscription_id
 }
 
-resource "github_actions_environment_secret" "azure_tenant_id" {
-  repository      = var.repository_name
-  secret_name     = "AZURE_TENANT_ID"
-  environment     = github_repository_environment.environment.environment
-  plaintext_value = var.tenant_id
+resource "github_actions_environment_variable" "azure_tenant_id" {
+  repository    = var.repository_name
+  variable_name = "AZURE_TENANT_ID"
+  environment   = github_repository_environment.environment.environment
+  value         = var.tenant_id
 }
