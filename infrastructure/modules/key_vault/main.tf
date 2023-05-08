756c9348296e6b48a032592a3f004c9fddb72164
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-08 22:22:11 +0200

    chore: re-add application id

--- infrastructure/modules/key_vault/main.tf
+++ infrastructure/modules/key_vault/main.tf
@@ -12,8 +12,9 @@ resource "azurerm_key_vault" "key_vault" {
 
   access_policy = [
     {
-      tenant_id = var.tenant_id
-      object_id = var.principal_id
+      application_id = var.application_id
+      tenant_id      = var.tenant_id
+      object_id      = var.principal_id
 
       key_permissions = [
         "Get", "List",
@@ -23,8 +24,9 @@ resource "azurerm_key_vault" "key_vault" {
       certificate_permissions = []
     },
     {
-      tenant_id = var.tenant_id
-      object_id = var.current_service_principal.object_id
+      tenant_id      = var.tenant_id
+      application_id = var.application_id
+      object_id      = var.current_service_principal.object_id
 
       key_permissions = [
         "Backup",
