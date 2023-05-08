f4461ca9e03cf7a385ad377bdbf9cae2409ed232
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-05-09 01:04:52 +0200

    🧱 chore(infra): provision preview deployments, add database (#31)
    * feat: add postgresql module
    
    * chore: manage apps, gh environments, work towards db
    
    * chore: debug
    
    * chore: add var file to plan
    
    * chore: unique key vault name
    
    * chore: shorter vault name
    
    * chore: add missing vars to production
    
    * chore: iam
    
    * chore: try to fix permissions
    
    * chore: stable locally
    
    * chore: debug logging
    
    * chore: skip manual login
    
    * chore: remove debug log
    
    * chore: create new application
    
    * chore: resource instead of data
    
    * chore: config azuread
    
    * Revert "chore: resource instead of data"
    
    This reverts commit 8f4e721583583d21aa40a1d0156575d1414d953f.
    
    * Revert "chore: create new application"
    
    This reverts commit ac81bcb0c9205d4f62b4616ee59281ed7178ffb1.
    
    * chore: add user assigned identities
    
    * chore: add user assigned identities
    
    * chore: secret access
    
    * chore: remove application, more permissive
    
    * chore: remove azuread
    
    * chore: add application id
    
    * chore: increase password length
    
    * chore: more complexity in password
    
    * fix: output value, not password id
    
    * chore: re-add data
    
    * chore: add sleep
    
    * chore: dashed name
    
    * chore: traditional access policy
    
    * chore: skip login
    
    * chore: skip manual login
    
    * chore: add missing properties
    
    * chore: add 10 sec delay
    
    * chore: add more permissions
    
    * chore: use client id for object id
    
    * chore: more debugging
    
    * chore: use application id
    
    * chore: application id
    
    * chore: remove application id
    
    * chore: re-add application id
    
    * chore: service prin object itd
    
    * chore: hard-code object id
    
    * chore: another hard-coded value
    
    * chore: access policy as list, use -or-create
    
    * chore: or-create=true
    
    * chore: -or-create in the correct place
    
    * chore: unique db name
    
    * chore: env variable instead of secret, add GH token
    
    * chore: add actions:write permission
    
    * chore: add comments to workflows
    
    * chore: test write-all permission
    
    * chore: test write-all permission
    
    * chore: skip plan for preview
    
    * chore: use pat
    
    * chore: less permission kv access policy
    
    * chore: terraform fmt
    
    * chore: format check
    
    * chore: skip format in preview deployments

---
+++ infrastructure/modules/postgres/variables.tf
@@ -0,0 +1,33 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to create the PostgreSQL Server."
+}
+
+variable "sku_name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "administrator_login_password" {
+  type        = string
+  description = "The password for the PostgreSQL Server administrator."
+  sensitive   = true
+}
+
+variable "administrator_login" {
+  type        = string
+  description = "The administrator login name for the PostgreSQL Server."
+}
+
+variable "storage_mb" {
+  type        = number
+  description = "The amount of storage allocated to this PostgreSQL Server."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the PostgreSQL Server."
+}
