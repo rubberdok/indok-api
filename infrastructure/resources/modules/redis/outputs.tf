cc3c9138d476a9edf70517d169ae1d9aefb3b852
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-10 12:34:58 +0200

    chore: prefix redis connection string with redis://

--- infrastructure/resources/modules/redis/outputs.tf
+++ infrastructure/resources/modules/redis/outputs.tf
@@ -1,4 +1,4 @@
 output "connection_string" {
-  value     = azurerm_redis_cache.this.primary_connection_string
+  value     = "redis://${azurerm_redis_cache.this.primary_connection_string}"
   sensitive = true
 }
