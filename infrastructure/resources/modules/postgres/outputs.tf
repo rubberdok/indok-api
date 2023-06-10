b989229d5f6883679f8d34ecc3aafca8b617613b
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-10 14:30:10 +0200

    chore: update db connection string

--- infrastructure/resources/modules/postgres/outputs.tf
+++ infrastructure/resources/modules/postgres/outputs.tf
@@ -1,4 +1,4 @@
 output "connection_string" {
-  value     = "postgresql://${var.administrator_login}:${var.administrator_password}@${azurerm_postgresql_flexible_server.this.fqdn}?sslmode=require&sslcert=./BaltimoreCyberTrustRoot.crt.pem"
+  value     = "postgresql://${var.administrator_login}:${var.administrator_password}@${azurerm_postgresql_flexible_server.this.fqdn}/postgres?sslmode=require&sslcert=./BaltimoreCyberTrustRoot.crt.pem"
   sensitive = true
 }
