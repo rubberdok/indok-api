7f67fba9202b15cc07738d170ed69cd08c5c360f
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-10 11:38:01 +0200

    chore: remove secret module

--- infrastructure/resources/modules/secret/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "postgres_password" {
-  value = {
-    name            = azurerm_key_vault_secret.postgres_password.name
-    plaintext_value = azurerm_key_vault_secret.postgres_password.value
-  }
-  sensitive = true
-}
---
+++ infrastructure/resources/secrets.tf
@@ -0,0 +1,26 @@
+resource "random_password" "password" {
+  length      = 128
+  special     = true
+  min_lower   = 1
+  min_numeric = 1
+  min_upper   = 1
+  min_special = 1
+}
+
+resource "azurerm_key_vault_secret" "postgres_password" {
+  name         = "postgres-password"
+  value        = random_password.password.result
+  key_vault_id = module.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "db_connection_string" {
+  name         = "db-connection-string"
+  value        = module.database.fqdn
+  key_vault_id = module.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "redis_connection_string" {
+  name         = "redis-connection-string"
+  value        = module.redis.fqdn
+  key_vault_id = module.key_vault.id
+}
