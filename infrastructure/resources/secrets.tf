c1f15c8632dcfa79b48bc39900e75b612d040a6b
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-10 12:31:16 +0200

    chore: url safe special characters

--- infrastructure/resources/secrets.tf
+++ infrastructure/resources/secrets.tf
@@ -1,10 +1,11 @@
 resource "random_password" "password" {
-  length      = 128
-  special     = true
-  min_lower   = 1
-  min_numeric = 1
-  min_upper   = 1
-  min_special = 1
+  length           = 128
+  special          = true
+  min_lower        = 1
+  min_numeric      = 1
+  min_upper        = 1
+  min_special      = 1
+  override_special = "-"
 }
 
 resource "azurerm_key_vault_secret" "postgres_password" {
