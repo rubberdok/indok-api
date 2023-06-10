be4fd32c24793f1619af8583c870c7cdaddad330
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-10 12:25:31 +0200

    chore: update connection strings

--- infrastructure/resources/modules/redis/outputs.tf
+++ infrastructure/resources/modules/redis/outputs.tf
@@ -1,4 +1,4 @@
-output "fqdn" {
+output "connection_string" {
   value     = azurerm_redis_cache.this.primary_connection_string
   sensitive = true
 }
--- infrastructure/resources/secrets.tf
+++ infrastructure/resources/secrets.tf
@@ -15,12 +15,12 @@ resource "azurerm_key_vault_secret" "postgres_password" {
 
 resource "azurerm_key_vault_secret" "db_connection_string" {
   name         = "db-connection-string"
-  value        = module.database.fqdn
+  value        = module.database.connection_string
   key_vault_id = module.key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "redis_connection_string" {
   name         = "redis-connection-string"
-  value        = module.redis.fqdn
+  value        = module.redis.connection_string
   key_vault_id = module.key_vault.id
 }
