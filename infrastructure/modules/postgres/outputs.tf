20ee1e478e9e54e96d4bb52d74ad91943d8f44bb
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-06-13 17:18:03 +0200

    chore: update connection string

--- infrastructure/modules/postgres/outputs.tf
+++ infrastructure/modules/postgres/outputs.tf
@@ -1,4 +1,4 @@
 output "connection_string" {
-  value     = "postgres://${var.administrator_login}:${var.administrator_password}@${azurerm_postgresql_flexible_server.this.fqdn}/postgres?sslmode=verify-full&sslrootcert=./DigiCertGlobalRootCA.crt.pem"
+  value     = "postgresql://${var.administrator_login}:${var.administrator_password}@${azurerm_postgresql_flexible_server.this.fqdn}/postgres?sslmode=require&sslcert=./DigiCertGlobalRootCA.crt.pem"
   sensitive = true
 }
