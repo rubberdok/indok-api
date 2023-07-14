516c41a27c02e3d11914d22253b10f0b88a35d48
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-07-14 16:25:14 +0200

    chore: remove env-specific variables

--- infrastructure/server/environments/production/github.tf
+++ infrastructure/server/environments/production/github.tf
@@ -8,14 +8,3 @@ resource "github_repository_environment" "this" {
   repository  = local.repository_name
 }
 
-resource "github_actions_environment_variable" "this" {
-  for_each = {
-    "azure_client_id"       = module.server.managed_identity.client_id,
-    "azure_tenant_id"       = module.server.managed_identity.tenant_id,
-    "azure_subscription_id" = data.azurerm_client_config.current.subscription_id,
-  }
-  repository    = local.repository_name
-  variable_name = upper(each.key)
-  environment   = github_repository_environment.this.environment
-  value         = each.value
-}
