dcc31fda883351bf147fd1077d7351290d187488
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-10-18 23:54:58 +0200

    :recycle: refactor: use url constructors for feide auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -3,21 +3,44 @@ import crypto from "crypto";
 import { User } from "@prisma/client";
 
 import { env } from "@/config.js";
-import { GetUserParams, IAuthService, IUserService } from "@/services/interfaces.js";
-
-import { OAuthClient, UserInfo } from "./interfaces.js";
+import { GetUserParams, IAuthService } from "@/services/interfaces.js";
 
 const FeideProvider = {
-  token: env.FEIDE_BASE_URL + "/oauth/token",
-  authorization: env.FEIDE_BASE_URL + "/oauth/authorization",
+  baseUrl: env.FEIDE_BASE_URL,
+  token: "/oauth/token",
+  authorization: "/oauth/authorization",
   redirectURL: env.FEIDE_REDIRECT_URI,
   clientID: env.FEIDE_CLIENT_ID,
-  userInfo: env.FEIDE_BASE_URL + "/openid/userinfo",
+  userInfo: "/openid/userinfo",
 };
+
+interface UserService {
+  create(data: {
+    email: string;
+    firstName: string;
+    lastName: string;
+    feideId: string;
+    username: string;
+  }): Promise<User>;
+  login(id: string): Promise<User>;
+  getByFeideID(feideId: string): Promise<User | null>;
+}
+
+interface OAuthClient {
+  fetchUserInfo(params: { url: string; accessToken: string }): Promise<UserInfo>;
+  fetchAccessToken(params: { url: string; body: URLSearchParams; authorization: string }): Promise<string>;
+}
+
+interface UserInfo {
+  sub: string;
+  name: string;
+  "dataporten-userid_sec": string[];
+  email: string;
+}
 
 export class FeideService implements IAuthService {
   constructor(
-    private userService: IUserService,
+    private userService: UserService,
     private oauthClient: OAuthClient
   ) {}
 
@@ -31,10 +54,22 @@ export class FeideService implements IAuthService {
     codeVerifier: string;
   } {
     const { codeVerifier, codeChallenge } = this.pkce();
+
+    const url = new URL(FeideProvider.authorization, FeideProvider.baseUrl);
+    const searchParams = new URLSearchParams({
+      client_id: FeideProvider.clientID,
+      scope: this.scope,
+      response_type: "code",
+      code_challenge_method: "S256",
+      code_challenge: codeChallenge,
+      redirect_uri: FeideProvider.redirectURL,
+      ...(state ? { state } : {}),
+    });
+
+    url.search = searchParams.toString();
+
     return {
-      url:
-        `${FeideProvider.authorization}?client_id=${env.FEIDE_CLIENT_ID}&scope=${this.scope}&response_type=code&code_challenge_method=S256&code_challenge=${codeChallenge}&redirect_uri=${env.FEIDE_REDIRECT_URI}` +
-        (state ? `&state=${state}` : ""),
+      url: url.toString(),
       codeVerifier,
       codeChallenge,
     };
@@ -64,13 +99,13 @@ export class FeideService implements IAuthService {
   }
 
   private async getUserInfo(accessToken: string): Promise<UserInfo> {
-    const url = FeideProvider.userInfo;
+    const url = new URL(FeideProvider.userInfo, FeideProvider.baseUrl).toString();
 
     return await this.oauthClient.fetchUserInfo({ url, accessToken });
   }
 
   private async getAccessToken(code: string, codeVerifier: string): Promise<string> {
-    const url = FeideProvider.token;
+    const url = new URL(FeideProvider.token, FeideProvider.baseUrl).toString();
 
     // https://en.wikipedia.org/wiki/Basic_access_authentication
     const authorization =
