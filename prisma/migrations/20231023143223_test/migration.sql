458c6c316d207658d7d94eb57d15f9e80c4e572d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-10-23 17:53:24 +0200

    chore: brutally remove migrations

--- prisma/migrations/20231023143223_test/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `test` to the `GoogleDocument` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "GoogleDocument" ADD COLUMN     "test" TEXT NOT NULL;
