aceb34f20e4eac6ed966e8c3282223605bd083fe
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-11-19 20:19:58 +0100

    chore: use clover code reporter

--- jest.config.cjs
+++ jest.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage/unit",
 
-  coverageReporters: ["lcov"],
+  coverageReporters: ["clover"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
