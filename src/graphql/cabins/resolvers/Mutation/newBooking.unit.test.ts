bb4f1015b8aa263f4a3835b6570cc360fce1bbd4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-11-20 09:18:04 +0100

    chore: update newBooking test

--- src/graphql/cabins/resolvers/Mutation/newBooking.unit.test.ts
+++ src/graphql/cabins/resolvers/Mutation/newBooking.unit.test.ts
@@ -4,12 +4,23 @@ import { mock } from "jest-mock-extended";
 
 import { createMockApolloServer } from "@/graphql/test-clients/mock-apollo-server.js";
 import { graphql } from "@/graphql/test-clients/unit/gql.js";
+import { NewBookingMutationVariables } from "@/graphql/test-clients/unit/graphql.js";
 
 describe("Cabin mutations", () => {
   describe("newBooking", () => {
     it("should create a new booking", async () => {
-      const { client, cabinService } = createMockApolloServer(console);
+      const { client, cabinService } = createMockApolloServer();
       cabinService.newBooking.mockResolvedValue(mock<Booking>({ id: faker.string.uuid() }));
+
+      const data: NewBookingMutationVariables["data"] = {
+        cabinId: faker.string.uuid(),
+        startDate: new Date(2020, 0, 1),
+        endDate: new Date(2020, 0, 2),
+        email: faker.internet.exampleEmail(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        phoneNumber: faker.phone.number(),
+      };
 
       const { errors } = await client.mutate({
         mutation: graphql(`
@@ -20,19 +31,20 @@ describe("Cabin mutations", () => {
           }
         `),
         variables: {
-          data: {
-            cabinId: faker.string.uuid(),
-            startDate: new Date(2020, 0, 1),
-            endDate: new Date(2020, 0, 2),
-            email: faker.internet.exampleEmail(),
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            phoneNumber: faker.phone.number(),
-          },
+          data,
         },
       });
 
       expect(errors).toBeUndefined();
+      expect(cabinService.newBooking).toHaveBeenCalledWith({
+        cabinId: data.cabinId,
+        startDate: data.startDate,
+        endDate: data.endDate,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        phoneNumber: data.phoneNumber,
+      });
     });
   });
 });
