a9817bf24af70ef02a951ee6d3cec1bd84d11117
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-11-25 22:29:56 +0100

    chore: add codecov config (#257)
    * chore: add codecov config
    
    * chore: assign prisma to global this in dev
    
    * chore: fix build command
    
    * chore: remove commnet

--- src/graphql/cabins/resolvers/Booking.ts
+++ src/graphql/cabins/resolvers/Booking.ts
@@ -1,6 +1,5 @@
 import type { BookingResolvers } from "./../../types.generated.js";
 export const Booking: BookingResolvers = {
-  /* Implement Booking resolver logic here */
   cabin: (parent, _arg, ctx) => {
     return ctx.cabinService.getCabin(parent.cabinId);
   },
