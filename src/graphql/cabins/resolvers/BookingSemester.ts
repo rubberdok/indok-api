2debc025f97de8c143c2a0f302f5d3b603fee20e
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-11-26 10:58:11 +0100

    :recycle: refactor: rename autumn to fall

--- src/graphql/cabins/resolvers/BookingSemester.ts
+++ src/graphql/cabins/resolvers/BookingSemester.ts
@@ -1,4 +1,8 @@
 import type { BookingSemesterResolvers } from "./../../types.generated.js";
 export const BookingSemester: BookingSemesterResolvers = {
   /* Implement BookingSemester resolver logic here */
+  semester: ({ semester }) => {
+    /* BookingSemester.semester resolver is required because BookingSemester.semester and BookingSemesterMapper.semester are not compatible */
+    return semester;
+  },
 };
--- src/graphql/cabins/resolvers/BookingSemestersResponse.ts
+++ src/graphql/cabins/resolvers/BookingSemestersResponse.ts
@@ -3,8 +3,8 @@ import { Semester } from "@prisma/client";
 import type { BookingSemestersResponseResolvers } from "./../../types.generated.js";
 export const BookingSemestersResponse: BookingSemestersResponseResolvers = {
   /* Implement BookingSemestersResponse resolver logic here */
-  autumn: async (_parent, _args, ctx) => {
-    return await ctx.cabinService.getBookingSemester(Semester.AUTUMN);
+  fall: async (_parent, _args, ctx) => {
+    return await ctx.cabinService.getBookingSemester(Semester.FALL);
   },
   spring: async (_parent, _args, ctx) => {
     return await ctx.cabinService.getBookingSemester(Semester.SPRING);
