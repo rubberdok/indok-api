d41cd9bd5482f065ff95cbd9ea3e52fc2bf85ddf
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-12-21 17:10:34 +0100

    chore: remove migration health check (#330)
    * chore: remove migration health check
    
    * chore: lint and format

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,49 +4,49 @@
  */
 
 module.exports = {
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+	// Automatically clear mock calls, instances, contexts and results before every test
+	clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false,
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage: false,
 
-  collectCoverageFrom: [
-    "src/**/*.{ts,js}",
-    "!**/test-clients/**",
-    "!**/__tests__/**",
-    "!**/__mocks__/**",
-    "!**/__types__.ts",
-    "!**/interfaces.ts",
-    "!**/seed.ts",
-    "!**/type-defs.ts",
-    "!src/graphql/types.generated.ts",
-    "!**/*.test.ts",
-  ],
+	collectCoverageFrom: [
+		"src/**/*.{ts,js}",
+		"!**/test-clients/**",
+		"!**/__tests__/**",
+		"!**/__mocks__/**",
+		"!**/__types__.ts",
+		"!**/interfaces.ts",
+		"!**/seed.ts",
+		"!**/type-defs.ts",
+		"!src/graphql/types.generated.ts",
+		"!**/*.test.ts",
+	],
 
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage/unit",
+	// The directory where Jest should output its coverage files
+	coverageDirectory: "coverage/unit",
 
-  coverageReporters: ["clover", "json", "lcov"],
+	coverageReporters: ["clover", "json", "lcov"],
 
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider: "v8",
 
-  // The glob patterns Jest uses to detect test files
-  testMatch: ["**/__tests__/unit/**/*.test.ts", "**/*.unit.test.ts"],
+	// The glob patterns Jest uses to detect test files
+	testMatch: ["**/__tests__/unit/**/*.test.ts", "**/*.unit.test.ts"],
 
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+	modulePathIgnorePatterns: ["<rootDir>/dist/"],
 
-  extensionsToTreatAsEsm: [".ts"],
+	extensionsToTreatAsEsm: [".ts"],
 
-  // https://github.com/swc-project/jest/issues/64
-  // imports with .js extension fails
-  // this is a workaround
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
+	// https://github.com/swc-project/jest/issues/64
+	// imports with .js extension fails
+	// this is a workaround
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
 
-  // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
-  },
+	// A map from regular expressions to paths to transformers
+	transform: {
+		"^.+\\.(t|j)sx?$": "@swc/jest",
+	},
 };
