2aff43c866485cdca6a03d5e286f7b075d851fcd
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-01-04 16:26:55 +0100

    chore: test initServer

--- src/__tests__/integration/server.test.ts
+++ src/__tests__/integration/server.test.ts
@@ -2,20 +2,39 @@ import { createServer, initServer } from "../../server.js";
 import { defaultTestDependenciesFactory } from "../dependencies-factory.js";
 
 describe("Server", () => {
-	let server: Awaited<ReturnType<typeof initServer>> | undefined;
+	describe("#createServer", () => {
+		let server: Awaited<ReturnType<typeof createServer>> | undefined;
 
-	beforeAll(async () => {
-		const dependencies = defaultTestDependenciesFactory();
-		server = await createServer(dependencies);
-	});
+		beforeAll(async () => {
+			const dependencies = defaultTestDependenciesFactory();
+			server = await createServer(dependencies);
+		});
 
-	test("Get data from server", async () => {
-		await server?.inject({ method: "GET", url: "/-/health" }).then((res) => {
-			expect(res.statusCode).toBe(200);
+		it("GET /-/health", async () => {
+			const res = await server?.inject({ method: "GET", url: "/-/health" });
+			expect(res?.statusCode).toBe(200);
+		});
+
+		afterAll(() => {
+			server?.close();
 		});
 	});
 
-	afterAll(() => {
-		server?.close();
+	describe("#initServer", () => {
+		let server: Awaited<ReturnType<typeof initServer>> | undefined;
+
+		beforeAll(async () => {
+			const dependencies = defaultTestDependenciesFactory();
+			server = await initServer(dependencies, { port: 4001, host: "0.0.0.0" });
+		});
+
+		it("GET /-/health should be available on port 4001", async () => {
+			const res = await fetch("http://localhost:4001/-/health");
+			expect(res.status).toBe(200);
+		});
+
+		afterAll(() => {
+			server?.close();
+		});
 	});
 });
