f217fca245e763a29db2915c02907984877ea5aa
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-01-09 23:29:56 +0100

    :construction: wip(users): add service and repo logic for study programs (#351)
    * chore: add study program to db schema
    
    * chore: add study program to user repository
    
    * chore: add study program to user service
    
    * chore: lint
    
    * chore: update imports and types

--- src/domain/users.ts
+++ src/domain/users.ts
@@ -1,6 +1,36 @@
 import type { User as PrismaUser } from "@prisma/client";
+import { z } from "zod";
+import { InvalidArgumentError } from "./errors.js";
 
 export interface User extends PrismaUser {
 	gradeYear?: number;
 	canUpdateYear: boolean;
+}
+
+export type StudyProgram = {
+	id: string;
+	name: string;
+	externalId: string;
+};
+
+export function newStudyProgram(studyProgram: {
+	name: string;
+	externalId: string;
+}): Omit<StudyProgram, "id"> {
+	const schema = z.object({
+		name: z.string().min(2),
+		externalId: z.string(),
+	});
+	try {
+		const { name, externalId } = schema.parse(studyProgram);
+		return {
+			name,
+			externalId,
+		};
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			throw new InvalidArgumentError(err.message);
+		}
+		throw err;
+	}
 }
