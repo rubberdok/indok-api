18d1cba6fa5b152be5f96458035eb0c6ae5bb7ef
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-02-13 14:52:13 +0100

    chore: add more tests, increase robustness

--- src/services/products/__tests__/integration/worker.test.ts
+++ src/services/products/__tests__/integration/worker.test.ts
@@ -1,13 +1,15 @@
 import { fail } from "assert";
 import { faker } from "@faker-js/faker";
+import { jest } from "@jest/globals";
 import { UnrecoverableError } from "bullmq";
-import { type DeepMockProxy, mockDeep } from "jest-mock-extended";
+import { type DeepMockProxy, mock, mockDeep } from "jest-mock-extended";
 import {
 	DownstreamServiceError,
 	InternalServerError,
 	InvalidArgumentError,
 	NotFoundError,
 } from "~/domain/errors.js";
+import type { PaymentAttemptType } from "~/domain/products.js";
 import type { ProductServiceType } from "../../service.js";
 import type {
 	PaymentProcessingQueueType,
@@ -31,6 +33,7 @@ describe("ProductService Worker", () => {
 
 	afterAll(async () => {
 		await close();
+		jest.clearAllMocks();
 	});
 
 	describe("capture-payment", () => {
@@ -40,9 +43,10 @@ describe("ProductService Worker", () => {
 				ok: false,
 				error: new InvalidArgumentError("Invalid argument"),
 			});
+			let actualError: Error | undefined = undefined;
 			worker.removeAllListeners();
 			worker.on("failed", (_job, err) => {
-				expect(err).toBeInstanceOf(UnrecoverableError);
+				actualError = err;
 			});
 			// Act
 			const job = await paymentProcessingQueue.add("capture-payment", {
@@ -51,9 +55,9 @@ describe("ProductService Worker", () => {
 			try {
 				await job.waitUntilFinished(queueEvents);
 				fail("Expected UnrecoverableError");
-			} catch (err) {
-				expect(err).toBeInstanceOf(Error);
-			}
+			} catch (err) {}
+			while (actualError === undefined) {}
+			expect(actualError).toBeInstanceOf(UnrecoverableError);
 		});
 
 		it("should throw UnrecoverableError if the capture returns InternalServerError", async () => {
@@ -62,9 +66,10 @@ describe("ProductService Worker", () => {
 				ok: false,
 				error: new InternalServerError("Internal server error"),
 			});
+			let actualError: Error | undefined = undefined;
 			worker.removeAllListeners();
 			worker.on("failed", (_job, err) => {
-				expect(err).toBeInstanceOf(UnrecoverableError);
+				actualError = err;
 			});
 			// Act
 			const job = await paymentProcessingQueue.add("capture-payment", {
@@ -73,9 +78,9 @@ describe("ProductService Worker", () => {
 			try {
 				await job.waitUntilFinished(queueEvents);
 				fail("Expected UnrecoverableError");
-			} catch (err) {
-				expect(err).toBeInstanceOf(Error);
-			}
+			} catch (err) {}
+			while (actualError === undefined) {}
+			expect(actualError).toBeInstanceOf(UnrecoverableError);
 		});
 
 		it("should throw NotFoundError if the capture returns NotFoundError", async () => {
@@ -84,9 +89,10 @@ describe("ProductService Worker", () => {
 				ok: false,
 				error: new NotFoundError("Not found"),
 			});
+			let actualError: Error | undefined = undefined;
 			worker.removeAllListeners();
 			worker.on("failed", (_job, err) => {
-				expect(err).toBeInstanceOf(NotFoundError);
+				actualError = err;
 			});
 			// Act
 			const job = await paymentProcessingQueue.add("capture-payment", {
@@ -94,9 +100,9 @@ describe("ProductService Worker", () => {
 			});
 			try {
 				await job.waitUntilFinished(queueEvents);
-			} catch (err) {
-				expect(err).toBeInstanceOf(Error);
-			}
+			} catch (err) {}
+			while (actualError === undefined) {}
+			expect(actualError).toBeInstanceOf(NotFoundError);
 		});
 
 		it("should move job to delayed if the capture returns a DownstreamServiceError", async () => {
@@ -108,6 +114,118 @@ describe("ProductService Worker", () => {
 			worker.removeAllListeners();
 			// Act
 			const job = await paymentProcessingQueue.add("capture-payment", {
+				reference: faker.string.uuid(),
+			});
+			let isDelayed = false;
+			while (!isDelayed) {
+				isDelayed = await job.isDelayed();
+			}
+			expect(isDelayed).toBe(true);
+		});
+	});
+
+	describe("payment-attempt-polling", () => {
+		it("should throw UnrecoverableError the payment attempt is null", async () => {
+			// Arrange
+			productService.payments.get.mockResolvedValue({
+				ok: true,
+				data: {
+					paymentAttempt: mock<PaymentAttemptType>(),
+				},
+			});
+			productService.payments.updatePaymentAttemptState.mockResolvedValue({
+				ok: false,
+				error: new NotFoundError("Not found"),
+			});
+
+			worker.removeAllListeners();
+			worker.on("failed", (_job, err) => {
+				expect(err).toBeInstanceOf(NotFoundError);
+			});
+			// Act
+			const job = await paymentProcessingQueue.add("payment-attempt-polling", {
+				reference: faker.string.uuid(),
+			});
+			try {
+				await job.waitUntilFinished(queueEvents);
+				fail("Expected UnrecoverableError");
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error);
+			}
+		});
+
+		it("should throw NotFoundError the update returns NotFoundError", async () => {
+			// Arrange
+			productService.payments.get.mockResolvedValue({
+				ok: true,
+				data: {
+					paymentAttempt: mock<PaymentAttemptType>(),
+				},
+			});
+			productService.payments.updatePaymentAttemptState.mockResolvedValue({
+				ok: false,
+				error: new NotFoundError("Not found"),
+			});
+
+			let actualError: Error | undefined = undefined;
+			worker.removeAllListeners();
+			worker.on("failed", (_job, err) => {
+				actualError = err;
+			});
+			// Act
+			const job = await paymentProcessingQueue.add("payment-attempt-polling", {
+				reference: faker.string.uuid(),
+			});
+			try {
+				await job.waitUntilFinished(queueEvents);
+			} catch (err) {}
+			while (actualError === undefined) {}
+			expect(actualError).toBeInstanceOf(NotFoundError);
+		});
+
+		it("should throw InternalServerError the update returns InternalServerError", async () => {
+			// Arrange
+			productService.payments.get.mockResolvedValue({
+				ok: true,
+				data: {
+					paymentAttempt: mock<PaymentAttemptType>(),
+				},
+			});
+			productService.payments.updatePaymentAttemptState.mockResolvedValue({
+				ok: false,
+				error: new InternalServerError("ISE"),
+			});
+			let actualError: Error | undefined = undefined;
+			worker.removeAllListeners();
+			worker.on("failed", (_job, err) => {
+				actualError = err;
+			});
+			// Act
+			const job = await paymentProcessingQueue.add("payment-attempt-polling", {
+				reference: faker.string.uuid(),
+			});
+			try {
+				await job.waitUntilFinished(queueEvents);
+			} catch (err) {}
+			while (actualError === undefined) {}
+			expect(actualError).toBeInstanceOf(InternalServerError);
+		});
+
+		it("should move job to delayed if a DownstreamServiceError is returned", async () => {
+			// Arrange
+			productService.payments.get.mockResolvedValue({
+				ok: true,
+				data: {
+					paymentAttempt: mock<PaymentAttemptType>(),
+				},
+			});
+			productService.payments.updatePaymentAttemptState.mockResolvedValue({
+				ok: false,
+				error: new DownstreamServiceError("Downstream"),
+			});
+			worker.removeAllListeners();
+			// Act
+			const job = await paymentProcessingQueue.add("payment-attempt-polling", {
 				reference: faker.string.uuid(),
 			});
 			let isDelayed = false;
