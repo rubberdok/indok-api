130be35b54c4013119589bef1009ea8bfc06b8fd
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-02-15 11:38:24 +0100

    chore: remove access control in gql

--- src/graphql/events/resolvers/SignUp.ts
+++ src/graphql/events/resolvers/SignUp.ts
@@ -21,8 +21,6 @@ export const SignUp: SignUpResolvers = {
 		return ctx.users.get(signUp.userId);
 	},
 	order: async (signUp, _args, ctx) => {
-		if (!ctx.user) return null;
-		if (ctx.user.id !== signUp.userId) return null;
 		const getOrderResult = await ctx.events.getOrderForSignUp(ctx, {
 			eventId: signUp.eventId,
 			userId: signUp.userId,
