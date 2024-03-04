4c7eb5db5029e434ed67bba77fc78b8425b9a5cf
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-03-04 18:44:28 +0100

    chore: set min tls version explicitly

--- infrastructure/modules/blob_storage/main.tf
+++ infrastructure/modules/blob_storage/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_account" "main" {
+  min_tls_version          = "TLS1_2"
   name                     = var.name
   resource_group_name      = var.resource_group.name
   location                 = var.resource_group.location
