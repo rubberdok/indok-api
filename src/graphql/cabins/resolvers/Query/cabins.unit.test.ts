18e1c1599d0f2e103674e037bb711d18544a7d72
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-03-07 19:54:06 +0100

    chore: add test for price resolver

--- src/graphql/cabins/resolvers/Query/cabins.unit.test.ts
+++ src/graphql/cabins/resolvers/Query/cabins.unit.test.ts
@@ -4,23 +4,22 @@ import { mock } from "jest-mock-extended";
 import { createMockApolloServer } from "~/graphql/test-clients/mock-apollo-server.js";
 import { graphql } from "~/graphql/test-clients/unit/gql.js";
 
-describe("Cabin queries", () => {
-	describe("cabins", () => {
-		it("returns all cabins", async () => {
-			const { client, cabinService } = createMockApolloServer();
-			cabinService.findManyCabins.mockResolvedValue([
-				mock<Cabin>({
-					id: faker.string.uuid(),
-					name: "Oksen",
-				}),
-				mock<Cabin>({
-					id: faker.string.uuid(),
-					name: "Bjørnen",
-				}),
-			]);
+describe("query { cabins } ", () => {
+	it("returns all cabins", async () => {
+		const { client, cabinService } = createMockApolloServer();
+		cabinService.findManyCabins.mockResolvedValue([
+			mock<Cabin>({
+				id: faker.string.uuid(),
+				name: "Oksen",
+			}),
+			mock<Cabin>({
+				id: faker.string.uuid(),
+				name: "Bjørnen",
+			}),
+		]);
 
-			const { errors } = await client.query({
-				query: graphql(`
+		const { errors } = await client.query({
+			query: graphql(`
           query cabins {
             cabins {
               cabins {
@@ -30,10 +29,52 @@ describe("Cabin queries", () => {
             }
           }
         `),
-			});
-
-			expect(errors).toBeUndefined();
-			expect(cabinService.findManyCabins).toHaveBeenCalled();
 		});
+
+		expect(errors).toBeUndefined();
+		expect(cabinService.findManyCabins).toHaveBeenCalled();
+	});
+
+	it("{ price } resolves price ", async () => {
+		const { client, cabinService } = createMockApolloServer();
+		cabinService.findManyCabins.mockResolvedValue([
+			{
+				id: faker.string.uuid(),
+				name: "Oksen",
+				capacity: 10,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				externalPrice: 1000,
+				externalPriceWeekend: 2000,
+				internalPrice: 500,
+				internalPriceWeekend: 750,
+			},
+		]);
+
+		const { errors } = await client.query({
+			query: graphql(`
+          query CabinsPrice {
+            cabins {
+              cabins {
+                id
+                name
+				price {
+					external {
+						weekend
+						weekend
+					}
+					internal {
+						weekend
+						weekday
+					}
+				}
+              }
+            }
+          }
+        `),
+		});
+
+		expect(errors).toBeUndefined();
+		expect(cabinService.findManyCabins).toHaveBeenCalled();
 	});
 });
