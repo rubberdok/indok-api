ec0f170f325bdcf84b3e507e5174b09c6366bedf
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-06-20 22:12:35 +0200

    chore: bump sentry to latest

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,12 @@
-import Prisma from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { env } from "~/config.js";
-
-const PrismaClient = Prisma.PrismaClient;
 
 /**
  * This is done to prevent hot reloading from creating new instances of PrismaClient in development
  * https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
  */
 const globalForPrisma = globalThis as unknown as {
-	prisma: Prisma.PrismaClient;
+	prisma: PrismaClient;
 };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
@@ -20,5 +18,3 @@ export const prismaKnownErrorCodes = {
 	ERR_NOT_FOUND: "P2025",
 	ERR_INCONSISTENT_COLUMN_DATA: "P2023",
 } as const;
-
-console.log("this runs as expected");
