9d81df144cffe891fe7759921eb679b79b7e5438
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-06-20 21:45:00 +0200

    chore: double the inactivity timeout to 10 seconds

--- src/__tests__/dev/dev.test.ts
+++ src/__tests__/dev/dev.test.ts
@@ -60,7 +60,7 @@ describe("Development scripts", () => {
 				const inactivityTimeoutHandle = setTimeout(() => {
 					console.log("Inactivity timeout reached, sending SIGTERM");
 					controller.abort();
-				}, 5 * 1000);
+				}, 10 * 1000);
 
 				/**
 				 * Instead of passing the signal to the process, we listen for the abort event
