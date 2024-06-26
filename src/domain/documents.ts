3127138f6f23220b4304c9e061f77a4a60f367de
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-06-26 21:29:14 +0200

    chore: update domain models

--- src/domain/documents.ts
+++ src/domain/documents.ts
@@ -1,102 +1,116 @@
 import type { Context } from "~/lib/context.js";
 import type { ResultAsync } from "~/lib/result.js";
 import type {
-	DownstreamServiceError,
-	InternalServerError,
-	InvalidArgumentErrorV2,
-	NotFoundError,
-	PermissionDeniedError,
-	UnauthorizedError,
+  DownstreamServiceError,
+  InternalServerError,
+  InvalidArgumentErrorV2,
+  NotFoundError,
+  PermissionDeniedError,
+  UnauthorizedError,
 } from "./errors.js";
 
 class DocumentCategory {
-	id: string;
-	name: string;
+  id: string;
+  name: string;
 
-	constructor(params: { id: string; name: string }) {
-		const { id, name } = params;
-		this.id = id;
-		this.name = name;
-	}
+  constructor(params: { id: string; name: string }) {
+    const { id, name } = params;
+    this.id = id;
+    this.name = name;
+  }
 }
 
 class Document {
-	name: string;
-	fileId: string;
-	createdAt: Date;
-	updatedAt: Date;
-	id: string;
-	description: string;
-	categories: DocumentCategory[];
+  name: string;
+  fileId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  id: string;
+  description: string;
+  categories: DocumentCategory[];
 
-	constructor(params: {
-		name: string;
-		fileId: string;
-		createdAt: Date;
-		updatedAt: Date;
-		id: string;
-		description?: string;
-		categories?: DocumentCategory[];
-	}) {
-		const { name, fileId, createdAt, updatedAt, id, description, categories } =
-			params;
-		this.name = name;
-		this.fileId = fileId;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-		this.id = id;
-		this.description = description ?? "";
-		this.categories = categories ?? [];
-	}
+  constructor(params: {
+    name: string;
+    fileId: string;
+    createdAt: Date;
+    updatedAt: Date;
+    id: string;
+    description?: string;
+    categories?: DocumentCategory[];
+  }) {
+    const { name, fileId, createdAt, updatedAt, id, description, categories } =
+      params;
+    this.name = name;
+    this.fileId = fileId;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.id = id;
+    this.description = description ?? "";
+    this.categories = categories ?? [];
+  }
 }
 
 type DocumentService = {
-	documents: {
-		create(
-			ctx: Context,
-			data: Pick<Document, "name"> & { fileExtension: string },
-		): ResultAsync<
-			{ document: Document; uploadUrl: string },
-			| InvalidArgumentErrorV2
-			| InternalServerError
-			| PermissionDeniedError
-			| UnauthorizedError
-			| DownstreamServiceError
-		>;
-		update(
-			ctx: Context,
-			data: Pick<Document, "id"> &
-				Partial<{
-					name: string | null;
-					description: string | null;
-					categories: { name: string }[] | null;
-				}>,
-		): ResultAsync<
-			{ document: Document },
-			| InvalidArgumentErrorV2
-			| InternalServerError
-			| PermissionDeniedError
-			| UnauthorizedError
-			| NotFoundError
-		>;
-		delete(
-			ctx: Context,
-			data: Pick<Document, "id">,
-		): ResultAsync<
-			{ document: Document },
-			| NotFoundError
-			| InternalServerError
-			| PermissionDeniedError
-			| UnauthorizedError
-		>;
-		findMany(
-			ctx: Context,
-			by?: { categories?: { id: string }[] | null } | null,
-		): ResultAsync<
-			{ documents: Document[]; total: number },
-			InternalServerError | PermissionDeniedError | UnauthorizedError
-		>;
-	};
+  documents: {
+    create(
+      ctx: Context,
+      data: Pick<Document, "name"> & { fileExtension: string },
+    ): ResultAsync<
+      { document: Document; uploadUrl: string },
+      | InvalidArgumentErrorV2
+      | InternalServerError
+      | PermissionDeniedError
+      | UnauthorizedError
+      | DownstreamServiceError
+    >;
+    update(
+      ctx: Context,
+      data: Pick<Document, "id"> &
+        Partial<{
+          name: string | null;
+          description: string | null;
+          categories: { name: string }[] | null;
+        }>,
+    ): ResultAsync<
+      { document: Document },
+      | InvalidArgumentErrorV2
+      | InternalServerError
+      | PermissionDeniedError
+      | UnauthorizedError
+      | NotFoundError
+    >;
+    delete(
+      ctx: Context,
+      data: Pick<Document, "id">,
+    ): ResultAsync<
+      { document: Document },
+      | NotFoundError
+      | InternalServerError
+      | PermissionDeniedError
+      | UnauthorizedError
+    >;
+    findMany(
+      ctx: Context,
+      by?: { categories?: { id: string }[] | null } | null,
+    ): ResultAsync<
+      { documents: Document[]; total: number },
+      InternalServerError | PermissionDeniedError | UnauthorizedError
+    >;
+    find(ctx: Context, params: { id: string }): ResultAsync<
+      { document: Document },
+      NotFoundError | InternalServerError | PermissionDeniedError | UnauthorizedError
+    >;
+  };
+  categories: {
+    findMany(ctx: Context): ResultAsync<
+      { categories: DocumentCategory[], total: number },
+      InternalServerError | PermissionDeniedError | UnauthorizedError
+    >;
+    delete(ctx: Context, params: { id: string }): ResultAsync<
+      { category: DocumentCategory },
+      NotFoundError | InternalServerError | PermissionDeniedError | UnauthorizedError
+    >;
+  }
 };
 
 export type { DocumentService };
